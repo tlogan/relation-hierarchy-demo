f6d0ba80250de83410201f103188fbc2176c23e5
Thomas Logan <thomaslilogan@gmail.com>
2014-02-25 13:09:55 -0500

    used table in organization settings

--- dragoman_io.js
+++ dragoman_io.js
@@ -1,7 +1,7 @@
 dragoman.io = function(){
 
-  var div = function(id) {
-    return $('<div id="' + id + '"></div>');
+  var div = function() {
+    return $('<div></div>');
   };
 
 
@@ -12,9 +12,10 @@ dragoman.io = function(){
       .css('margin', 0);
 
     var mod_height = '50px';
+    var text_color = '#88c';
 
     var panel = function(id) {
-      return div(id).attr('class', 'panel')
+      return div().attr('id', id).attr('class', 'panel')
         .css('width', '400px')
         .css('height', '100%')
         .css('background-color', '#ddd')
@@ -24,39 +25,71 @@ dragoman.io = function(){
     };
 
     var panel_item = function(id) {
-      return div(id).attr('class', 'panel_item')
+      return div().attr('id', id).attr('class', 'panel_item')
         .css('background-color', '#eef')
         .css('border-bottom', '2px solid #ccc')
         .css('border-left', '2px solid #9d9')
-        .css('cursor', 'pointer')
       ;
     };
 
     var text_item = function(text) {
-      return $('<div></div>')
+      return div() 
         .text(text)
-        .css('color', '#88c')
-        .css('padding-left', '15')
+        .css('color', text_color)
+        .css('padding-top', '15px')
+        .css('padding-left', '15px')
       ;
+    };
+
+    var table = function() {
+      return $('<table></table>');
+    };
+
+    var td = function() {
+      return $('<td></td>').css('color', text_color);
+    };
+
+    var tr = function() {
+      return $('<tr></tr>');
     };
 
     var mod_panel_item = function(id, text) {
       return panel_item(id)
+        .css('cursor', 'pointer')
         .css('height', mod_height)
-        .append(text_item(text)
-          .css('padding-top', '15')
-        )
+        .append(text_item(text))
       ;
     };
 
+    var text_input = function() {
+      return $('<input type="text"/>');
+    };
+
     var organization_settings_item = function() {
+
       return panel_item('organization_settings')
-        .append(text_item('message organization:')) 
-        .append(text_item('anchor as')) 
-        .append(text_item('group by')) 
-        .append(text_item('filter where')) 
-        .append(text_item('display as')) 
-      ;
+        .css('padding-top', '15px')
+        .css('padding-bottom', '15px')
+        .append(text_item('message organization:'))
+        .append(function() {
+          var t = table() 
+            .css('padding-left', '15px');
+          _.forEach([
+            ['anchor as', text_input()],
+            ['group by', ''] ,
+            ['filter where', ''], 
+            ['display as', ''] 
+          ], function(item) {
+            t.append(
+              tr().append(
+                td().text(item[0])
+              ).append(
+                td().append(item[1])
+              )
+            );
+          }); 
+          return t;
+        })
     };
 
     var add_panel = function(panel) {
@@ -78,7 +111,7 @@ dragoman.io = function(){
         .css('color', '#fff')
         .css('background-color', '#ccc')
         .css('height', '100%')
-        .append(div('control_bar')
+        .append(div().attr('id', 'control_bar')
           .css('height', mod_height)
           .css('background-color', '#9d9')
         )
